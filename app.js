989f2fe609455b0121aad33d8113df9a159d7896
Aaron200497 <98113453+Aaron200497@users.noreply.github.com>
2025-03-19 17:10:18 +0100

    app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let categorias = [];
 
 // Cargar datos del Excel "stock.xlsx" (se añade parámetro para evitar caché)
 function loadExcelData() {
-  fetch('stock.jsont=' + new Date().getTime())
+  fetch('stock.json=' + new Date().getTime())
     .then(response => response.arrayBuffer())
     .then(arrayBuffer => {
       const data = new Uint8Array(arrayBuffer);
